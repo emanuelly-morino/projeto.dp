1a092cb96fee08981be323b7a18c07bd98fb59ce
emanuelly-morino <manu.hmorino@gmail.com>
2025-03-26 11:10:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".swiper", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  keyboard: true,
+});
+
+function openPage(pageUrl) {
+    window.location.href = pageUrl; 
+}
